5589b8e0b58ec1e0e92bdc2e1b2524c03307a4fb
Changbeom HAN <xhxkf0202@naver.com>
2024-08-05 09:45:32 +0900

    [level 1] Title: 폰켓몬, Time: 3.80 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        int k = nums.length / 2;
-        for(int x : nums) {
-            set.add(x);
+        HashSet<Integer> hashSet = new HashSet<>();
+        int answer = 0;
+        for(int i = 0; i < nums.length; i++) {
+            hashSet.add(nums[i]);
         }
-        if(set.size() >= k) return k;
-        else    return set.size();
+        if(hashSet.size() >= nums.length / 2)   answer = nums.length / 2;
+        else    answer = hashSet.size();
+        return answer;
     }
 }
