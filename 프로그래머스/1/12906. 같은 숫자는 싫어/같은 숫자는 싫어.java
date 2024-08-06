79d167ab8c283e06eab8318739ebfbe47bb7e94c
Changbeom HAN <xhxkf0202@naver.com>
2024-08-06 11:27:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 69.42 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,31 +2,31 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
+        int index = 0;
+        
         ArrayList<Integer> list = new ArrayList<>();
-        Queue<Integer> Q = new LinkedList<>();
-        for(int i = 0; i < arr.length; i++)
-        {
-            if(Q.isEmpty()){
-                Q.offer(arr[i]);
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[index]);
+        index++;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(stack.isEmpty()) {
+                stack.push(arr[i]);
             }
             else {
-                if(Q.peek() != arr[i]){
-                    list.add(Q.poll());
-                    Q.offer(arr[i]);
+                if(stack.peek() == arr[i]) {
+                    continue;
+                }
+                else {
+                    list.add(stack.pop());
+                    stack.push(arr[i]);
                 }
             }
         }
-        if(!Q.isEmpty()) {
-            list.add(Q.poll());
-        }
         
-        int[] answer = new int[list.size()];
-        int i = 0;
-        for(int x : list){
-            answer[i] = x;
-            i++;
+        if(!stack.isEmpty()){
+            list.add(stack.pop());
         }
-        System.out.print(list.size());
-        return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
