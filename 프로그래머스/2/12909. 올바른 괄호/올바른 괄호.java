dedc1baf071a56b3f95ca80fd54116a1d6d43c62
Changbeom HAN <xhxkf0202@naver.com>
2024-08-07 13:48:34 +0900

    [level 2] Title: 올바른 괄호, Time: 22.95 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,18 +1,29 @@
 import java.util.*;
 
 class Solution {
-    boolean solution(String s) {
+    public boolean solution(String s) {
         boolean answer = true;
-        Stack<Character> st = new Stack<>();
-        char[] arr = s.toCharArray();
-        for(char c : arr) {
-            if(c == '(')   st.push('(');
-            else{
-                if(st.isEmpty())    return false;
-                else    st.pop();
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(Character ch : s.toCharArray()) {
+            if(ch == '(') {
+                stack.push(ch);
+                continue;
+            }
+            if(ch == ')') {
+                if(stack.size() == 0) {
+                    return false;
+                }
+                if(stack.peek() == '(') {
+                    stack.pop();
+                }
             }
         }
-        if(st.isEmpty())    return answer;
-        else    return false;
+        
+        while(!stack.isEmpty()){
+            return false;
+        }
+            return true;
     }
 }
