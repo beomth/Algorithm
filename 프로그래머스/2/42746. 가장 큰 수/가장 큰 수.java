6afbb4f29068353303b71a881be78b8622a4597d
Changbeom HAN <xhxkf0202@naver.com>
2024-08-12 15:56:49 +0900

    [level 2] Title: 가장 큰 수, Time: 212.28 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,25 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
         String[] str = new String[numbers.length];
         
-        //int 배열을 String배열로 변환
         for(int i = 0; i < numbers.length; i++) {
             str[i] = String.valueOf(numbers[i]);
         }
         
-        // 내림차순 정렬
         Arrays.sort(str, new Comparator<String>() {
-            @Override
-            public int compare(String a, String b) {
-                return (b+a).compareTo(a+b);
-            }
+           public int compare(String a, String b) {
+               return (b+a).compareTo(a+b);
+           } 
         });
         
-        if(str[0].equals("0"))  return "0";
+        if(str[0].equals("0"))    return "0";
+        StringBuilder sb = new StringBuilder();
         
-        for(String s : str) answer += s;
-        return answer;
+        for(int i = 0; i < str.length; i++) {
+            sb.append(str[i]);
+        }
+        return sb.toString();
     }
 }
