66a3801b023c9e95bb4e08559645cc666a908451
Changbeom HAN <xhxkf0202@naver.com>
2024-08-19 22:47:05 +0900

    [level 1] Title: 체육복, Time: 0.08 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,34 +3,38 @@ import java.util.HashSet;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int[] student = new int[n+2];
+        // 가장 앞과 뒤는 0으로 초기화
         student[0] = 0;
         student[n+1] = 0;
-
+        
         for (int i : reserve) {
-            student[i]++;
+            student[i] = 1;
         }
+        
         for (int i : lost) {
-            student[i]--;
+            student[i] -= 1;
         }
-
-        for (int j = 1;  j < student.length; j++ ) {
-            if (student[j] == 1) {
-                if (student[j - 1] == -1) {
-                    student[j - 1]++;
-                    student[j]--;
-                } else if (student[j + 1] == -1) {
-                    student[j+1]++;
-                    student[j]--;
+        
+        for (int j = 1; j < student.length; j++) {
+            if(student[j] == 1) {
+                if(student[j-1] == -1) {
+                    student[j-1] += 1;
+                    student[j] -= 1;
+                }
+                else if(student[j+1] == -1) {
+                    student[j+1] += 1;
+                    student[j] -= 1;
                 }
             }
         }
+        
         int cnt = 0;
         for (int k : student) {
-            if (k < 0) {
-                cnt++;
+            if(k < 0) {
+                cnt++;            
             }
         }
+    
         return n - cnt;
-
     }
 }
