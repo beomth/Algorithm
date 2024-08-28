47f1e81d42f2442091dbb8ecef974e6392fc1366
Changbeom HAN <xhxkf0202@naver.com>
2024-08-28 16:10:12 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 92 ms, Memory: 12848 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("SciComLove");
+        }
+        else{
+            System.out.println("evoLmoCicS");
+        }
+    }
+}
